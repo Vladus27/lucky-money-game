c5e431b3a9c631560d307923289d26385c8661fe
Vlad <hellomyvolodia@outlook.com>
2026-01-01 20:53:39 +0200

    add inputs validation

--- lucky_money_app/lib/features/authentication/widgets/password_input.dart
+++ lucky_money_app/lib/features/authentication/widgets/password_input.dart
@@ -17,9 +17,21 @@ class PasswordInput extends StatelessWidget {
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
     return TextFormField(
+      maxLength: 64,
       keyboardType: TextInputType.text,
       controller: passwordController,
       obscureText: !isPasswordVisible,
+      validator: (value) {
+        if (value == null || value.isEmpty) {
+          return 'а пароль вписати?';
+        }
+        final regex = RegExp(r'^\S{8,64}$');
+        if (!regex.hasMatch(value)) {
+          return 'мінімум 8 симовлів без пробілів';
+        }
+
+        return null;
+      },
       decoration: InputDecoration(
         icon: const Icon(Icons.password_outlined),
         hintText: 'Enter your password',
@@ -32,6 +44,7 @@ class PasswordInput extends StatelessWidget {
         ),
         filled: true,
         fillColor: theme.colorScheme.surfaceContainer,
+
         enabledBorder: OutlineInputBorder(
           borderRadius: BorderRadius.circular(12),
           borderSide: BorderSide(color: theme.colorScheme.onPrimary),
--- lucky_money_app/lib/features/authentication/widgets/username_input.dart
+++ lucky_money_app/lib/features/authentication/widgets/username_input.dart
@@ -13,7 +13,19 @@ class UsernameInput extends StatelessWidget {
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
     return TextFormField(
+      maxLength: 20,
       controller: usernameController,
+      validator: (value) {
+        if (value == null || value.isEmpty) {
+          return 'Введи нікнейм';
+        }
+        final regex = RegExp(r'^[a-z0-9_]{3,20}$');
+        if (!regex.hasMatch(value)) {
+          return 'мін 3 символи: a-z, 0-9 або _';
+        }
+
+        return null;
+      },
       decoration: InputDecoration(
         icon: const Icon(Icons.person_outline_outlined),
         labelText: usernameLabel,
