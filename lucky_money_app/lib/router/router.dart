0379994ae0df49dba92e3767cfd34e42ff0c3673
Vlad <hellomyvolodia@outlook.com>
2026-01-02 13:21:48 +0200

    add notif router

--- lucky_money_app/lib/router/router.dart
+++ lucky_money_app/lib/router/router.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+
 import 'package:lucky_money_app/common/models/authentication_model.dart';
 import 'package:lucky_money_app/features/authentication/view/authentication_screen.dart';
+import 'package:lucky_money_app/features/history/view/history_screen.dart';
 import 'package:lucky_money_app/features/home/view/home_screen.dart';
 
 final Map<String, Widget Function(BuildContext)> routes = {
@@ -10,5 +12,5 @@ final Map<String, Widget Function(BuildContext)> routes = {
   '/auth-register': (ctx) =>
       const AuthenticationScreen(initialMode: AuthMode.register),
   '/wallet': (ctx) => const HomeScreen(), // temp home page
-  '/notif': (ctx) => const HomeScreen(), // temp home page
+  '/notif': (ctx) => const HistoryScreen(),
 };
