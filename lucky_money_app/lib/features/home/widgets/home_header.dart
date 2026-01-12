337ea42a46f2648ad0cefee7384e63208abe3b9d
Vlad <hellomyvolodia@outlook.com>
2026-01-12 12:55:59 +0200

    check auth user header via token

--- lucky_money_app/lib/features/home/widgets/home_header.dart
+++ lucky_money_app/lib/features/home/widgets/home_header.dart
@@ -2,32 +2,39 @@ import 'package:flutter/material.dart';
 import 'package:lucky_money_app/common/constant/image_constants.dart';
 import 'package:lucky_money_app/features/home/widgets/home_header_guest.dart';
 import 'package:lucky_money_app/features/home/widgets/home_header_logged.dart';
+import 'package:lucky_money_app/repo/secure_storage_service.dart';
 
 class HomeHeader extends StatelessWidget implements PreferredSizeWidget {
   const HomeHeader({super.key});
 
   @override
   Widget build(BuildContext context) {
-    bool isLogined = 1 == 2;
-    Widget userAction = isLogined
-        ? const HomeHeaderLogged()
-        : const HomeHeaderGuest();
+    final storage = SecureStorageService();
+    return FutureBuilder(
+      future: storage.getToken(),
+      builder: (context, asyncSnapshot) {
+        final bool isAuthenticated = asyncSnapshot.data != null;
+        Widget userAction = isAuthenticated
+            ? const HomeHeaderLogged()
+            : const HomeHeaderGuest();
 
-    return AppBar(
-      toolbarHeight: kToolbarHeight + 42,
-      leadingWidth: 96,
-      leading: Align(
-        alignment: Alignment.centerLeft,
-        child: Padding(
-          padding: const EdgeInsets.only(left: 12.0),
-          child: Image.asset(
-            appLogo,
-            height: double.infinity,
-            fit: BoxFit.contain,
+        return AppBar(
+          toolbarHeight: kToolbarHeight + 42,
+          leadingWidth: 96,
+          leading: Align(
+            alignment: Alignment.centerLeft,
+            child: Padding(
+              padding: const EdgeInsets.only(left: 12.0),
+              child: Image.asset(
+                appLogo,
+                height: double.infinity,
+                fit: BoxFit.contain,
+              ),
+            ),
           ),
-        ),
-      ),
-      actions: [userAction],
+          actions: [userAction],
+        );
+      },
     );
   }
 
