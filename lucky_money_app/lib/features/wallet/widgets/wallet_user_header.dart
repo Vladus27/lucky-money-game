afabadfe42ed3ea4419e4af7bd2dca1f7ffe992f
Vlad <hellomyvolodia@outlook.com>
2026-01-16 17:14:41 +0200

    impemented APIs for username in wallet header

--- lucky_money_app/lib/features/wallet/widgets/wallet_user_header.dart
+++ lucky_money_app/lib/features/wallet/widgets/wallet_user_header.dart
@@ -1,14 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:lucky_money_app/common/constant/image_constants.dart';
 import 'package:lucky_money_app/common/models/wallet_section_copy.dart';
+import 'package:lucky_money_app/providers/user_provider.dart';
 
-class WalletUserHeader extends StatelessWidget {
+class WalletUserHeader extends ConsumerWidget {
   const WalletUserHeader({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     final theme = Theme.of(context);
     final wallet = WalletSectionCopy.start();
+    final asyncUsername = ref.watch(userProvider);
+    String username = '';
+
+    asyncUsername.when(
+      loading: () => const CircularProgressIndicator(),
+      error: (_, __) => const Text('Щось пішло не так'),
+      data: (userResult) {
+        if (!userResult.isSuccess) {
+          WidgetsBinding.instance.addPostFrameCallback((_) {
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(
+                behavior: SnackBarBehavior.floating,
+                content: Text(userResult.error!.message),
+              ),
+            );
+          });
+        } else {
+          username = userResult.data!.username;
+        }
+      },
+    );
     return Padding(
       padding: const EdgeInsets.only(bottom: 16.0),
       child: Row(
@@ -26,7 +49,7 @@ class WalletUserHeader extends StatelessWidget {
                 style: const TextStyle(color: Colors.white),
               ),
               Text(
-                wallet.username!,
+                '@$username',
                 style: theme.textTheme.titleMedium!.copyWith(
                   fontWeight: FontWeight.w700,
                   color: theme.colorScheme.surface,
