d0e01099fa4aa5e0757f5fe4a12862ebc0535a00
Vlad <hellomyvolodia@outlook.com>
2026-01-16 16:34:03 +0200

    implemented balance APIs in wallet cadrs

--- lucky_money_app/lib/features/wallet/widgets/wallet_balance_card.dart
+++ lucky_money_app/lib/features/wallet/widgets/wallet_balance_card.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
 import 'package:lucky_money_app/common/models/wallet_section_copy.dart';
 import 'package:lucky_money_app/common/widgets/box_shadow.dart';
+import 'package:lucky_money_app/providers/user_provider.dart';
 
-class WalletBalanceCard extends StatelessWidget {
+class WalletBalanceCard extends ConsumerWidget {
   const WalletBalanceCard({
     super.key,
     required this.wallet,
@@ -12,9 +15,35 @@ class WalletBalanceCard extends StatelessWidget {
   final Color colorHead;
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final asyncBalance = ref.watch(balanceProvider);
+    String balanceText = '--.--';
+
+    asyncBalance.when(
+      loading: () => const CircularProgressIndicator(),
+      error: (_, __) => const Text('Помилка балансу'),
+      data: (balanceResult) {
+        if (!balanceResult.isSuccess) {
+          WidgetsBinding.instance.addPostFrameCallback((_) {
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(
+                behavior: SnackBarBehavior.floating,
+                content: Text(balanceResult.error!.message),
+              ),
+            );
+          });
+        } else {
+          balanceText = balanceResult.data!;
+        }
+      },
+    );
+
     final theme = Theme.of(context);
     final walletCopy = WalletSectionCopy.of(wallet);
+
+    if (wallet == WalletSection.inGameBalance) {
+      balanceText = walletCopy.balance!;
+    }
 
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 24.0),
@@ -57,7 +86,7 @@ class WalletBalanceCard extends StatelessWidget {
                 Center(
                   child: Padding(
                     padding: const EdgeInsets.all(8.0),
-                    child: Text(walletCopy.balance!),
+                    child: Text(balanceText),
                   ),
                 ),
               ],
