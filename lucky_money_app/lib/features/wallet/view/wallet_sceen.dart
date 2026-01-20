47438a0cf5d8d8024e5cd08a568c396b179cfd59
Vlad <hellomyvolodia@outlook.com>
2026-01-20 03:15:12 +0200

    add button in wallet screen to update the balance

--- lucky_money_app/lib/features/wallet/view/wallet_sceen.dart
+++ lucky_money_app/lib/features/wallet/view/wallet_sceen.dart
@@ -1,17 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'package:lucky_money_app/common/models/wallet_section_copy.dart';
 import 'package:lucky_money_app/features/wallet/widgets/wallet_balance_card.dart';
 import 'package:lucky_money_app/features/wallet/widgets/wallet_header.dart';
 import 'package:lucky_money_app/features/wallet/widgets/wallet_hero_section.dart';
+import 'package:lucky_money_app/providers/user_provider.dart';
 
-class WalletScreen extends StatelessWidget {
+class WalletScreen extends ConsumerWidget {
   const WalletScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     final colorTheme = Theme.of(context).colorScheme;
     return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          ref.invalidate(balanceProvider);
+        },
+        child: const Icon(Icons.refresh_outlined),
+      ),
       appBar: const WalletHeader(),
       body: SingleChildScrollView(
         child: Column(
