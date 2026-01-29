b02184cc68fbf766bfc695e323f98c3f726958d4
Vlad <hellomyvolodia@outlook.com>
2026-01-29 14:34:26 +0200

    improve revealBomb model

--- lucky_money_app/lib/common/models/game/reveal_bomb.dart
+++ lucky_money_app/lib/common/models/game/reveal_bomb.dart
@@ -2,13 +2,15 @@ class RevealBomb {
   final bool isBomb;
   final double currentMultiplier;
   final double currentCashoutAmount;
-  final Set<int>? minePositions;
+  final Set<int> minePositions;
+  final double nextMultiplier;
 
   const RevealBomb({
     required this.isBomb,
     required this.currentMultiplier,
     required this.currentCashoutAmount,
     required this.minePositions,
+    required this.nextMultiplier,
   });
 
   factory RevealBomb.fromJson(Map<String, dynamic> json) {
@@ -18,8 +20,9 @@ class RevealBomb {
       currentMultiplier: json['currentMultiplier'] as double,
       currentCashoutAmount: json['currentCashoutAmount'] as double,
       minePositions: rawMines == null
-          ? null
+          ? {}
           : (rawMines as List).map((e) => e['positionId'] as int).toSet(),
+      nextMultiplier: json["nextMultiplier"],
     );
   }
 }
